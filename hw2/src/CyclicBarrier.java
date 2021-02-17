b82210352a930dd3026f6fc361f8aec206b90e13
tylerpak <paktyler@utexas.edu>
2021-02-17 13:05:02 -0600

    Implemented CyclicBarrier, seems to work but will do further testing

--- hw2/src/CyclicBarrier.java
+++ hw2/src/CyclicBarrier.java
@@ -1,16 +1,30 @@
 //UT-EID= tjp2365, lwz83
 
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
+import java.util.concurrent.TimeUnit;
 
 public class CyclicBarrier {
+	private int waiting;
+	private int threshold;
+	public Semaphore sem;
 	
 	public CyclicBarrier(int parties) {
+		this.sem = new Semaphore(0, true);
+		this.waiting = 0;
+		this.threshold = parties;
 	}
 	
 	public int await() throws InterruptedException {
-           int index = 0;
-		
-          // you need to write this code
-	    return index;
+		int index = threshold - 1;
+		index = index - waiting;
+
+		waiting +=1;
+		if(waiting == threshold) {
+			sem.release(threshold);
+			waiting = 0;
+		}
+		sem.acquire();
+		TimeUnit.NANOSECONDS.sleep(10);
+		return index;
 	}
 }
